1785fd14a7c810ecf7ece042556453ea4482c286
randomowo <ne@randomowo.ru>
2019-01-18 10:38:30 +0300

    merged with dev branch

--- src/main.py
+++ src/main.py
@@ -10,13 +10,30 @@ EMPTY = [{}, [{}, 0.0]]
 
 
 def account(name):
+    """returns empty account with inputed name
+    
+    Arguments:
+        name {str} -- name for account
+    
+    Returns:
+        dict -- empty account
+    """
     return {name: [0.0, {TODAY: []}]}
 
 
 def borrow(arr):
-    if (len(arr) == 3):
+    """adds new information about borrows
+    
+    Arguments:
+        arr {list} -- inputed information
+    """
+    amount = arr[-1]
+    if (len(arr) == 3 and isnum(amount)):
         obj_list = load()[1]
-        obj_list[0].update({arr[-2]: arr[-1]})
+        if (arr[-2] not in obj_list[0]):
+            obj_list[0].update({arr[-2]: float(amount)})
+        else:
+            obj_list[0][arr[-2]] += float(amount)
         obj_list[1] += float(arr[-1])
         save_object(obj_list, 1)
     else:
@@ -24,6 +41,11 @@ def borrow(arr):
 
 
 def new(arr):
+    """create a new account
+    
+    Arguments:
+        arr {list} -- inputed command
+    """
     if (len(arr) == 2):
         obj_list = load()[0]
         if obj_list is None:
@@ -35,60 +57,87 @@ def new(arr):
 
 
 def remove(arr):
-    if (len(arr) >= 2):
-        obj_list = load()
-        del obj_list[arr[1]]
+    """remove from data
+    
+    Arguments:
+        arr {list} -- inputed command
+    """
+    obj_list = load()
+    if (len(arr) == 2 and arr[1] in obj_list[0]):
+        del obj_list[0][arr[1]]
         save(obj_list)
     else:
         print("err")
 
 
 def changeto(arr):
-    if (len(arr) == 3):
-        name = arr[0]
-        amount = arr[-1]
-        obj_list = load()
-        diff = obj_list[name][0] - float(amount)
+    """Change saving amount on account 
+
+    Arguments:
+        arr {list} -- input commands
+    """
+    obj_list = load()
+    name = arr[0]
+    amount = arr[-1]
+    if (len(arr) == 3 and name in obj_list[0] and isnum(amount)):
+        diff = obj_list[0][name][0] - float(amount)
         if (diff == 0):
             return
-        obj_list[name][0] = float(amount)
-        if obj_list[name][0] - float(amount) > 0:
-            obj_list[name][1][TODAY].append(diff)
+        obj_list[0][name][0] = float(amount)
+        if obj_list[0][name][0] - float(amount) > 0:
+            obj_list[0][name][1][TODAY].append(diff)
         else:
-            obj_list[name][1][TODAY].append(-diff)
+            obj_list[0][name][1][TODAY].append(-diff)
         save(obj_list)
     else:
         print("err")
 
 
 def plus_or_minus(arr, pm):
-    if (len(arr) == 3):
-        name = arr[0]
-        amount = arr[-1]
+    """increases or decreases saving on account
+    
+    Arguments:
+        arr {list} -- inputed command
+        pm {int} -- flag that says which operetion to do
+    """
+    obj_list = load()
+    name = arr[0]
+    amount = arr[-1]
+    if (len(arr) == 3 and name in obj_list[0] and isnum(amount)):
         if (amount == 0):
             return
         obj_list = load()
-        obj_list[name][1][TODAY].append(float(amount) * pm)
-        obj_list[name][0] += float(amount) * pm
+        obj_list[0][name][1][TODAY].append(float(amount) * pm)
+        obj_list[0][name][0] += float(amount) * pm
         save(obj_list)
     else:
         print("err")
 
 
 def to(arr):
-    new_arr = arr[::2] + [arr[-1]]
-    plus_or_minus(new_arr, -1)
-    new_arr = arr[-2::-2] + [arr[-1]]
-    plus_or_minus(new_arr, 1)
+    """transfer savings between account
+
+    Returns:
+        list -- inputed commands
+    """
+    plus_or_minus(arr[::2] + [arr[-1]], -1)
+    plus_or_minus(arr[-2::-2] + [arr[-1]], 1)
 
 
-def info(list, dflag, accflag):
-    if (len(list[1][0]) != 0 and dflag):
+def info(list, binfo, accflag):
+    """print stored information about finances
+    
+    Arguments:
+        list {dict} -- stored information
+        binfo {bool} -- flag that says print information only about borrows
+        accflag {bool} -- flag that says print information only about account
+    """
+    if (len(list[1][0]) != 0 and binfo):
         print("duties amout: ", list[1][1])
         print("duties list:")
         for duty_name, duty_value in list[1][0].items():
             print("\t", duty_name, " : ", duty_value)
-        if (dflag and not accflag):
+        if (binfo and not accflag):
             return
 
     if (len(list[0]) == 0 or not accflag):
@@ -97,12 +146,30 @@ def info(list, dflag, accflag):
 
     for acc_key, acc_value in list[0].items():
         print("Account name:", acc_key, "Money: ", acc_value[0])
-        print("diffs:", end='')
+        print("\tdiffs:", end='')
         for in_key, in_value in acc_value[1].items():
             print("\t", in_key, " : ", in_value)
 
 
+def isnum(value):
+    """chech is value is number
+    
+    Arguments:
+        value {any} -- any value
+    
+    Returns:
+        bool -- if value is float then True else False
+    """
+    try:
+        float(value)
+        return True
+    except BaseException:
+        return False
+
+
 def help():
+    """print information about command.
+    """
     print("""
         fin [command] [[other]]
             COMMANDS:
@@ -127,8 +194,8 @@ def help():
                 finfo - display information of all account and thier differences and information of borrows
                     finfo
 
-                dinfo - display information of borrows
-                    dinfo
+                binfo - display information of borrows
+                    binfo
 
                 ainfo - display information of all account and thier differences
                     ainfo
@@ -142,17 +209,33 @@ def help():
 
 
 def save_object(obj, type):
+    """saves object into file
+    
+    Arguments:
+        obj {dict} -- object to save 
+        type {int} -- flag that says which data type save
+    """
     obj_list = load()
     obj_list[type] = obj
     save(obj_list)
 
 
 def save(data):
+    """saves data in file
+    
+    Arguments:
+        data {list} -- all data
+    """
     with open(FILEPATH, "wb") as output:
         pickle.dump(data, output)
 
 
 def load():
+    """load data from file
+    
+    Returns:
+        list -- data from file
+    """
     try:
         with open(FILEPATH, "rb") as input:
             obj_list = pickle.load(input)
@@ -161,52 +244,40 @@ def load():
     return obj_list
 
 
-inp = sys.argv
-del inp[0]
-ACCOUNT_LIST = load()[0]
-DUTY_LIST = load()[1]
-inp_len = len(inp)
+command = {
+    "help": lambda: help(),
+    "clear": lambda: save(EMPTY),
+    "ainfo": lambda: info(load(), False, True),
+    "binfo": lambda: info(load(), True, False),
+    "finfo": lambda: info(load(), True, True),
+    "new": lambda: new(inp),
+    "remove": lambda: remove(inp),
+    "borrow": lambda: borrow(inp),
+    "changeto": lambda: changeto(inp),
+    "plus": lambda: plus_or_minus(inp, 1),
+    "minus": lambda: plus_or_minus(inp, -1)
+}
 
 
-if (inp_len == 0):
-    exit()
+def main():
+    """checks inputed command and then run needed function
+    """
+    inp = sys.argv[1::]
+    inp_len = len(inp)
 
-elif (inp[-inp_len] == "help"):
-    help()
+    if (inp_len == 0):
+        exit()
 
-elif (inp[-inp_len] == "clear"):
-    save(EMPTY)
+    if (inp[-inp_len] in command):
+        command[inp[-inp_len]]()
 
-elif (inp[-inp_len] == "ainfo"):
-    info(load(), False, True)
+    elif (inp_len > 2):
 
-elif (inp[-inp_len] == "dinfo"):
-    info(load(), True, False)
+        if (inp[-inp_len + 1] in command):
+            command[inp[-inp_len + 1]]()
 
-elif (inp[-inp_len] == "finfo"):
-    info(load(), True, True)
+    else:
+        print("err")
 
-elif (inp[-inp_len] == "new"):
-    new(inp)
 
-elif (inp[-inp_len] == "remove"):
-    remove(inp)
-
-elif(inp_len > 2):
-    if (inp[-inp_len] in ACCOUNT_LIST):
-        if (inp[-inp_len + 1] == "changeto"):
-            changeto(inp)
-
-        elif (inp[-inp_len + 1] == "plus"):
-            plus_or_minus(inp, 1)
-
-        elif (inp[-inp_len + 1] == "minus"):
-            plus_or_minus(inp, -1)
-
-        elif (inp[-inp_len + 1] == "to" and inp[-inp_len + 2] in ACCOUNT_LIST):
-            to(inp)
-
-    elif (inp[-inp_len] == "borrow"):
-        borrow(inp)
-else:
-    print("err")
+main()
